8476a2922e4e386b7ca9b3077dcf06a59c27dc0a
alevar <alevar@gmail.com>
2017-03-13 18:36:09 -0400

    modified for efficiency and accepting cram

--- rnaseq_al_pipe.sh
+++ rnaseq_al_pipe.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# This test is dedicated to correctly recognizing and conditionally processing .cram/.sam/.bam input alignment info
+
 input=$1
+fname=$(basename "$input")
+ext="${fname##*.}"
+nameM="${fname%.*}"
+echo $ext
+echo $nameM
 output=$2
 sf=$3
 rep=$4
+reference=$5
+GTFFILE=$6
 
 echo "+++++++++++++++++++++++++++++++"
 echo "+++++++++++++++++++++++++++++++"
@@ -68,32 +77,60 @@ pipeline() {
 
 echo [`date +"%Y-%m-%d %H:%M:%S"`] "#> START: " $0 $SCRIPTARGS
 
-
+isample=$(basename ${input})
 sample=$input
+
 echo [`date +"%Y-%m-%d %H:%M:%S"`] "   * Alignments conversion (SAMTools)"
 if [[ "$newsamtools" ]]; then
+ 
+ if [[ "$ext" = "sam" ]]; then
+    echo "sam"
+    $SAMTOOLS view -h -s ${sf} -S -b ${TEMPLOC}/${nameM}.sam | \
+     $SAMTOOLS sort -@ $NUMCPUS -o ${ALIGNLOC}/${nameM}.bam -
 
- isample=$(basename ${input})
- sample="${isample%%.*}"
-
- cp $input ${TEMPLOC}
-
- $SAMTOOLS view -h -s ${sf} -S -b ${TEMPLOC}/${sample}.sam | \
-  $SAMTOOLS sort -@ $NUMCPUS -o ${ALIGNLOC}/${sample}.bam -
+ elif [[ "$ext" = "bam" ]]; then
+    echo "bam"
+ elif [[ "$ext" = "cram" ]]; then
+    echo "cram"
+    if [ ! -f ${output}.sam ]; then
+        $SAMTOOLS view -h -T ${reference} -o ${output}.sam ${input}
+    fi
+    $SAMTOOLS view -h -s ${sf} -S -b ${output}.sam | \
+     $SAMTOOLS sort -@ $NUMCPUS -o ${ALIGNLOC}/${nameM}.bam -
 else
- $SAMTOOLS view -S -b ${TEMPLOC}/${sample}.sam | \
-  $SAMTOOLS sort -@ $NUMCPUS - ${ALIGNLOC}/${sample}
+    echo "unknown fileformat. please provide .sam/.bam/.cram"
+ fi
+   
+else
+ if [[ "$ext" = "sam" ]]; then
+    echo "sam"
+    $SAMTOOLS view -h -s ${sf} -S -b ${TEMPLOC}/${nameM}.sam | \
+     $SAMTOOLS sort -@ $NUMCPUS -o ${ALIGNLOC}/${nameM}.bam -
+
+ elif [[ "$ext" = "bam" ]]; then
+    echo "bam"
+ elif [[ "$ext" = "cram" ]]; then
+    echo "cram"
+     if [ ! -f ${output}.sam ]; then
+        $SAMTOOLS view -h -T ${reference} -o ${output}.sam ${input}
+    fi
+    $SAMTOOLS view -h -s ${sf} -S -b ${output}.sam | \
+     $SAMTOOLS sort -@ $NUMCPUS -o ${ALIGNLOC}/${nameM}.bam -
+else
+    echo "unknown fileformat. please provide .sam/.bam/.cram"
+ fi
 fi
+
 #$SAMTOOLS index ${ALIGNLOC}/${sample}.bam
 #$SAMTOOLS flagstat ${ALIGNLOC}/${sample}.bam
 
 #echo "..removing intermediate files"
-#rm ${TEMPLOC}/${sample}.sam
+# rm ${TEMPLOC}/*
 #rm ${TEMPLOC}/${sample}.unsorted.bam
 
 echo [`date +"%Y-%m-%d %H:%M:%S"`] "   * Assemble transcripts (StringTie)"
-$STRINGTIE -p $NUMCPUS -G ${GTFFILE} -o ${ALIGNLOC}/${sample}.gtf \
- -l ${sample} ${ALIGNLOC}/${sample}.bam
+$STRINGTIE -p $NUMCPUS -G ${GTFFILE} -o ${ALIGNLOC}/${nameM}.gtf \
+ -l ${sample} ${ALIGNLOC}/${nameM}.bam
 
 ## merge transcript file
 echo [`date +"%Y-%m-%d %H:%M:%S"`] "#> Merge all transcripts (StringTie)"
@@ -116,9 +153,9 @@ if [ ! -d ${BALLGOWNLOC}/${dsample} ]; then
    mkdir -p ${BALLGOWNLOC}/${dsample}
 fi
 $STRINGTIE -e -B -p $NUMCPUS -G ${BALLGOWNLOC}/stringtie_merged.gtf \
--o ${BALLGOWNLOC}/${dsample}/${dsample}.gtf ${ALIGNLOC}/${sample}.bam
+-o ${BALLGOWNLOC}/${dsample}/${dsample}.gtf ${ALIGNLOC}/${nameM}.bam
 
-awk -F '\t|;|"| ' '$3 == "transcript" && match($19,/reference_id*/) {print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$11"\t"$16"\t"$21"\t"$26"\t"$31"\t"$36"\t"$41"\t"$46}' ${ALIGNLOC}/${sample}.gtf > ${ALIGNLOC%/*}/transcripts.gtf
+awk -F '\t|;|"| ' '$3 == "transcript" && match($19,/reference_id*/) {print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$11"\t"$16"\t"$21"\t"$26"\t"$31"\t"$36"\t"$41"\t"$46}' ${ALIGNLOC}/${nameM}.gtf > ${ALIGNLOC%/*}/transcripts.gtf
 
 echo [`date +"%Y-%m-%d %H:%M:%S"`] "#> DONE."
 } #pipeline end
