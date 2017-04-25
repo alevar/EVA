418eb6d395e315d7b080c9a616736af203add797
Ales Varabyou <alevar@gmail.com>
2017-04-24 21:49:47 -0400

    refactored out plotting. housekeeping

--- Analyze.py
+++ Analyze.py
@@ -18,6 +18,7 @@ from ast import literal_eval
 from sklearn import preprocessing
 from scipy.stats import boxcox
 from pandas.tools.plotting import scatter_matrix
+from matplotlib import animation
 
 # lets also calculate pearson r correlation coefficient
 def pearson(dataT):
@@ -76,6 +77,7 @@ def KendalTau(df,dfBASE,topF=1.0,orderTop=True):
     return tau["rank"]["1.0"]
 
 def readStatsSF(path,outDir):
+    print("> Begin grouping transcripts by downsampling factor")
     dtypeC={'refID':'object',
             'tissue':'object',
             'sample':'int',
@@ -104,6 +106,7 @@ def readStatsSF(path,outDir):
     # First reject all transcripts for which tpm==0.0 at sf==1.0
     dataOff = pd.unique(data[(data["sf"] == 1.0) & (data["tpm"] == 0.0)]["ID"])
     uniqueID1Maintain = pd.unique(data[~data["ID"].isin(dataOff)]["ID"])
+    del dataOff
     dataN = data[~data["ID"].isin(uniqueID1Maintain)]
     data = data[data["ID"].isin(uniqueID1Maintain)]
     data["lost"] = data.apply(lambda row: row["tpm"] == 0.0,axis=1)
@@ -113,19 +116,35 @@ def readStatsSF(path,outDir):
     setYes = set(dataN[(dataN["sf"]==1.0)&(dataN["tpm"]!=0.0)]["ID"].unique()) # Lets try counting the number of novel mistake
     setFin = list(setNo.difference(setYes)) # Lets try counting the number of novel mistake
     dataT = dataN[dataN['ID'].isin(setFin)]
+    del dataN
     dataSF = pd.DataFrame(dataT.groupby(['sf']).mean()).reset_index()
+    print("> Begin counting false positives")
     dataSF["falsePositives"] = dataSF.apply(lambda row: len(dataT[(dataT['ID'].isin(setFin))&(dataT["tpm"]!=0.0)&(dataT["sf"]==row["sf"])]),axis=1)
+    print("< Done counting false positives")
     # 2 Here we shall also count the number of losses
+    print("> Begin counting false negatives")
     dataSF["falseNegatives"] = dataSF.apply(lambda row: len(data[(data["sf"] == row["sf"])&(data["lost"])]),axis=1)
+    print("< Done counting false negatives")
     # 2 here we shall add information about total losses
     dataLostAll = pd.DataFrame(data.groupby(["ID","sf"]).mean()).reset_index()
     dataSF["falseNegativesFull"] = dataSF.apply(lambda row: len(dataLostAll[(dataLostAll["sf"] == row["sf"])&(dataLostAll["lost"] == 1.0)]),axis=1)
     # 3 Calculating the total number of transcripts at a particular coverage point
     dataSF["NumTranscripts"] = pd.DataFrame(data.groupby(["sf"],as_index=False)["tpm"].count())["tpm"]
     # First we need to express tpm as percentage deviation from the baseTPM
-    # data["percentAway"] = data.apply(lambda row: row["tpm"]/data[(data["ID"] == row['ID'])&(data["sf"] == 1.0)]["tpm"],axis=1)
     dictBase = pd.Series(data[data["sf"]==1.0].tpm.values,index=data[data["sf"]==1.0].ID).to_dict()
+    print("> Begin calculating percent away from the original tpm estimation")
+    # dataBASE = pd.DataFrame([])
+    # dataBASE[["ID","baseTPM"]] = data[data["sf"]==1.0][["ID","tpm"]]
+    # data = pd.merge(data, dataBASE,on='ID',how='outer')
+    # data = data.drop_duplicates()
+    # del dataBASE
+    # data = data.drop("tpm_y",axis=1)
+    # data.columns = ["tissue","chr","refID","tID","start","end","sample","sf","cov","tpm","ID","lost","baseTPM","percentAway"]
+    # data["percentAway"] = (data["tpm"]/data["baseTPM"])*100
+    # data.to_csv(outDir+"/csv/raw.csv")
     data["percentAway"] = data.apply(lambda row: (row["tpm"]/dictBase[row["ID"]])*100 if row["ID"] in dictBase else np.nan,axis=1)
+    print("< Done calculating percent away from the original tpm estimation")
+    print("> Begin calculating quartiles, mean and whiskers")
     dataSF["q25"] = pd.DataFrame(data.groupby(["sf"])["percentAway"].quantile(0.25)).reset_index()["percentAway"]
     dataSF["median"] = pd.DataFrame(data.groupby(["sf"])["percentAway"].quantile(0.50)).reset_index()["percentAway"]
     dataSF["q75"] = pd.DataFrame(data.groupby(["sf"])["percentAway"].quantile(0.75)).reset_index()["percentAway"]
@@ -133,7 +152,9 @@ def readStatsSF(path,outDir):
     dataSF["mean"] = pd.DataFrame(data.groupby(["sf"])["percentAway"].mean()).reset_index()["percentAway"]
 
     dataSF[['whiskLow','whiskHigh','extremes']] = pd.DataFrame([x for x in dataSF.apply(lambda row: calcWhisk(row,data),axis=1)])
+    print("< Done calculating quartiles, mean and whiskers")
     # now it's time to count the number of outliers for each sf
+    print("> Begin counting, weighing and normalizing counts for outliers")
     dataSF["numExtremes"] = dataSF.apply(lambda row: len(row["extremes"]),axis=1)
     # now we shall weigh the number of extremes by the mean distance from the median point at each sf
     # the reason mean is chosen (and not the median) is because we want to assign weight based on how
@@ -141,11 +162,15 @@ def readStatsSF(path,outDir):
     dataSF["weightedNumExtremes"] = dataSF.apply(lambda row: 0 if row["sf"] == 1.0 else abs(np.array(row["extremes"])-row["median"]).mean()*row["numExtremes"],axis=1)
     # weighted extremes normalized by number of transcripts in the bin
     dataSF["weightedNormalizedNumExtremes"] = dataSF.apply(lambda row: row["weightedNumExtremes"]/row["NumTranscripts"],axis=1)
+    print("< Done counting, weighing and normalizing counts for outliers")
     # add standard deviation and coefficient of variation
+    print("> Begin calculating std and cv")
     dataSF["std"] = pd.DataFrame(data.groupby(["sf"])["percentAway"].std()).reset_index()["percentAway"]
     dataSF["cv"] = dataSF.apply(lambda row: (row["std"]/row['mean'])*100,axis=1)
+    print("< Done calculating std and cv")
     # STRATEGY 3: Time to include kendal tau ranking correlation results for each sf-base pair
     # create an ID for distinguishing between samples
+    print("> Begin Ranking and Tau coeeficient calculation")
     data["RankSampleID"] = data["ID"]+":"+data["sample"].astype(str)
 
     dataSF["tauFull"] = dataSF.apply(lambda row: KendalTau(data[data["sf"] == row["sf"]][["RankSampleID","tpm"]],data[data["sf"] == 1.0][["RankSampleID","tpm"]],1.0,True),axis=1)
@@ -155,6 +180,7 @@ def readStatsSF(path,outDir):
     dataSF["tauBottom10"] = dataSF.apply(lambda row: KendalTau(data[data["sf"] == row["sf"]][["RankSampleID","tpm"]],data[data["sf"] == 1.0][["RankSampleID","tpm"]],0.1,False),axis=1)
     dataSF["tauBottom20"] = dataSF.apply(lambda row: KendalTau(data[data["sf"] == row["sf"]][["RankSampleID","tpm"]],data[data["sf"] == 1.0][["RankSampleID","tpm"]],0.2,False),axis=1)
     dataSF["tauBottom50"] = dataSF.apply(lambda row: KendalTau(data[data["sf"] == row["sf"]][["RankSampleID","tpm"]],data[data["sf"] == 1.0][["RankSampleID","tpm"]],0.5,False),axis=1)
+    print("< Done Ranking and Tau coeeficient calculation")
     dataSF[["sf",
             "cov",
             "tpm",
@@ -176,39 +202,16 @@ def readStatsSF(path,outDir):
             "tauTop10",
             "tauTop20",
             "tauTop50",
+            "tauBottom10",
             "tauBottom20",
             "tauBottom50"]].to_csv(outDir+"/csv/groupedBySF.csv")
-
-    return dataSF
-
-def plotBoxSF(data,outDir,res):
-    # First we plotted the median, q25 and q75
-    plt.close('all')
-    plt.clf()
-    plt.figure(figsize=(int(res[0]), int(res[1])))
-    plt.title('Normalized TPM Deviation')
-    plt.plot(data["sf"], data["median"],'k',color='#CC4F1B')
-        
-    plt.fill_between(data["sf"], data["q25"], data["q75"],
-        alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
-    plt.fill_between(data["sf"], data["whiskLow"], data["whiskHigh"],
-        alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
-    plt.savefig(outDir+"/png/boxSF.png")
-    plt.close('all')
-    plt.clf()
-
-def plotTauSF(data,outDir,res):
-    # Next we plotted all kendal tau coefficients
-    ax=data[["tauFull","tauTop10","tauTop20","tauTop50","tauBottom50","tauBottom20","tauBottom10"]].plot(x=data["sf"],subplots=True, layout=(1, 7), figsize=(int(res[0]), int(res[1])), sharex=False, sharey=True);
-    plt.savefig(outDir+"/png/tauSF.png")
-
-def plotScattermatrixSF(data,outDir,res):
-    # Then we created a scatter matrix of each quantitative measure with density plots on the diagonal
-    # The data was visually inspected to determine any signs of linearity
-    ax=sbn.pairplot(data[['sf','falsePositives','falseNegatives','falseNegativesFull','std','tauFull','median']])
-    plt.savefig(outDir+"/png/scatterMatrixSF.png")
+    
+    del dataSF
+    # del data
+    print("< Done grouping transcripts by downsampling factor")
 
 def readStatsID(path,outDir):
+    print("> Begin grouping transcripts by unique ID")
     dtypeC={'refID':'object',
             'tissue':'object',
             'sample':'int',
@@ -237,6 +240,7 @@ def readStatsID(path,outDir):
     unique=dataPrime["sf"].unique().tolist()
     frames = []
     for sf in unique[:-1]:
+        print(sf)
         df = pd.DataFrame([])
         dfTPM = pd.DataFrame([])
         df[["ID","covBase","tpmBase","sample"]] = dataPrime[(dataPrime["sf"]==1.0)&~(dataPrime["cov"]==0.0)].reset_index().drop("index",1)[["ID","cov","tpm",'sample']]
@@ -278,69 +282,12 @@ def readStatsID(path,outDir):
     data=pd.concat(frames)
     data=data.reset_index(drop=True)
     data.to_csv(outDir+"/csv/groupedByID.csv")
-    return data
-
-def plotAll(data,outDir,res):
-    eDF = pd.DataFrame(data["ID"])
-    for cl in list(data)[1:-1]:
-        eDF[str(cl)] = pd.DataFrame(preprocessing.scale(boxcox(data[cl]+1)[0]))
-    eDF["sf"]=data['sf']
-
-    unique=data["sf"].unique().tolist()
-    for sf in unique:
-        dataTMP = data[data["sf"]==sf]
-        ax=sbn.pairplot(eDF[eDF["sf"]==sf][['covBase','tpmBase','falseNegative','tpmMEAN','tpmSTD','tpmQ25','tpmQ50','tpmQ75','tpmCV','tpmIQR']])
-        ax.savefig(outDir+"/png/"+str(sf)+"scatterMatrixByIDTransformed.png")
-        
-        # lets try identifying upper outliers in covBase
-        q25,q50,q75 = dataTMP['covBase'].quantile([0.25,0.5,0.75])
-        iqr = q75-q25
-        thw = q75+0.2*iqr
-        ahw = dataTMP[dataTMP["covBase"]<thw]["covBase"].max()
-        dataTMP2 = dataTMP[(data['covBase']<q75)&(dataTMP['covBase']>q25)]
-
-        plt.close("all")
-        
-        ax=sbn.pairplot(dataTMP[['covBase','tpmBase','falseNegative','tpmMEAN','tpmSTD','tpmQ25','tpmQ50','tpmQ75','tpmCV','tpmIQR']])
-        ax.savefig(outDir+"/png/"+str(sf)+"scatterMatrixByID.png")
-
-        plt.close('all')
-        ax=eDF[eDF["sf"]==sf][["covBase","tpmQ50"]].plot(x="covBase",y="tpmQ50")
-        plt.savefig(outDir+"/png/"+str(sf)+"boxIDTransformed.png")
-        
-        plt.close("all")
-        ax=eDF[eDF["sf"]==sf][['covBase','tpmBase','falseNegative','tpmMEAN','tpmSTD','tpmQ25','tpmQ50','tpmQ75','tpmCV','tpmIQR']].plot(x=eDF[eDF["sf"]==sf]["covBase"],subplots=True, layout=(4, 4), figsize=(int(res[0]), int(res[1])), sharex=False)
-        plt.savefig(outDir+"/png/"+str(sf)+"groupedIDTransformed.png")
-
-        plt.close('all')
-        plt.figure(figsize=(75,75))
-        plt.title('Normalized TPM Deviation')
-        plt.plot(dataTMP2["covBase"], dataTMP2["tpmQ50"],'k',color='#CC4F1B')
-        plt.fill_between(dataTMP2["covBase"], dataTMP2["tpmQ25"], dataTMP2["tpmQ75"],
-            alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
-        plt.savefig(outDir+"/png/"+str(sf)+"boxID.png")
-
-        plt.close("all")
-        ax=dataTMP2[['covBase','tpmBase','falseNegative','tpmMEAN','tpmSTD','tpmQ25','tpmQ50','tpmQ75','tpmCV','tpmIQR']].plot(x=dataTMP2["covBase"],subplots=True, layout=(4, 4), figsize=(int(res[0]), int(res[1])), sharex=False)
-        plt.savefig(outDir+"/png/"+str(sf)+"groupedID.png")
-        
-        del dataTMP
-        del ax
-        del dataTMP2
+    print("< Done grouping transcripts by unique ID")
 
 def main(args):
     if not os.path.exists(os.path.abspath(args.out)):
         os.makedirs(os.path.abspath(args.out))
-        os.makedirs(os.path.abspath(args.out)+'/png')
         os.makedirs(os.path.abspath(args.out)+'/csv')
 
-    dataSF = readStatsSF(os.path.abspath(args.input),os.path.abspath(args.out))
-    plotBoxSF(dataSF,os.path.abspath(args.out),args.resolution.split(":"))
-    plotTauSF(dataSF,os.path.abspath(args.out),args.resolution.split(":"))
-    plotScattermatrixSF(dataSF,os.path.abspath(args.out),args.resolution.split(":"))
-
-    dataID = readStatsID(os.path.abspath(args.input),os.path.abspath(args.out))
-    plotAll(dataID,os.path.abspath(args.out),args.resolution.split(":"))
-
-    R_CMD = "Rscript ./pca.r "+os.path.abspath(args.out)
-    os.system(R_CMD)
+    readStatsSF(os.path.abspath(args.input),os.path.abspath(args.out))
+    readStatsID(os.path.abspath(args.input),os.path.abspath(args.out))
