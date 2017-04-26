61ff6d093213eab04314cf68aa47958cdec0b50c
Ales Varabyou <alevar@gmail.com>
2017-04-26 16:57:08 -0400

    refined axis, ticks, recall/precision,etc...

--- Plot.py
+++ Plot.py
@@ -22,37 +22,57 @@ from matplotlib import animation
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA as sklearnPCA
 
-spotsOriginal = 92884446.8 # eventuall will be suplied by the program
+spotsOriginal = 92884447 # Mean number of spots in the selected alignments. eventually will be suplied by the program
 
 def plotBoxSF(data,outDir,res):
-    # First we plotted the median, q25 and q75
+    # First we plotted the median and quartiles
     plt.close('all')
     plt.clf()
-    plt.figure(figsize=(int(res[0]), int(res[1])))
-    plt.title('Change variation of transcript expression (%TPM) estimation as a function of the portion of aligned spots')
-    plt.ylabel('Deviation of expression estimate from control (% TPM)')
-    plt.xlabel("Portion of aligned spots used in assembly. Full alignment contained "+str(spotsOriginal)+" spots")
-    plt.plot(data["sf"], data["median"],'k',color='#CC4F1B')
-    plt.fill_between(data["sf"], data["q25"], data["q75"],
+    fig = plt.figure(figsize=(int(res[0]),int(res[1])))
+    ax1 = fig.add_axes((0.1,0.25,0.8,0.7))
+    ax1.set_title('Change in variation of transcript expression (%TPM) estimation as a function of the portion of aligned spots')
+    ax1.set_ylabel('Deviation of expression estimate from control (% TPM)')
+    ax1.set_xlabel("Portion of aligned spots")
+    ax1.plot(data["sf"], data["median"],'k',color='#CC4F1B')
+    ax1.set_xlim(data["sf"].min(),data["sf"].max())
+    ax1.set_xticks(data["sf"])
+    ax1.fill_between(data["sf"], data["q25"], data["q75"],
         alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
-    plt.fill_between(data["sf"], data["whiskLow"], data["whiskHigh"],
+    ax1.fill_between(data["sf"], data["whiskLow"], data["whiskHigh"],
         alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
 
+    ax2 = fig.add_axes((0.1,0.2,0.8,0.0))
+    ax2.set_xlim(data["sf"].min(),data["sf"].max())
+    ax2.yaxis.set_visible(False)
+    ax2.set_xticks(data["sf"])
+    ax2.set_xlabel("# aligned spots")
+    ticks2F = [str('%.2f' %((elem*spotsOriginal)/1000000))+"M" for elem in data["sf"].tolist()]
+    ax2.set_xticklabels(ticks2F)
+
     caption = "Figure. Plot shows the change in variation of estimated transcript expression levels (%TPM) as a function of the number of aligned reads used in the assembly. The plot shows the median and four quartiles of the distribution of estimated expression levels (%TPM) for each downsampling."
-    plt.figtext(.05, .05, caption,wrap=True,fontsize=18)
+    # plt.figtext(.05, .05, caption,wrap=True,fontsize=18)
     plt.savefig(outDir+"/png/boxSF.png")
     plt.close('all')
     plt.clf()
 
 def plotTauSF(data,outDir,res):
     # Next we plotted all kendal tau coefficients
+    plt.close('all')
+    plt.clf()
+    fig = plt.figure(figsize=(int(res[0]),int(res[1])))
+    ax1 = fig.add_axes((0.1,0.25,0.8,0.7))
     title = "Portion of aligned spots versus Kendall's Tau ranking of transcripts by expression levels"
-    ax=data[["sf","tauFull","tauTop10","tauTop20","tauTop50","tauBottom50","tauBottom20","tauBottom10"]].plot(x="sf",subplots=False, figsize=(int(res[0]), int(res[1])), title=title);
-    ax.set_xlabel("Portion of aligned spots used in assembly. Full alignment contained "+str(spotsOriginal)+" spots")
-    ax.set_ylabel("Kendall's Tau ranking correlation coefficient")
+    ax1.set_title(title)
+    ax1.set_ylabel("Kendall's Tau ranking correlation coefficient")
+    ax1.set_xlabel("Portion of aligned spots")
+    for tauSeries in data[["tauFull","tauTop10","tauTop20","tauTop50","tauBottom50","tauBottom20","tauBottom10"]]:
+        ax1.plot(data["sf"], data[tauSeries],label=tauSeries)
+    ax1.legend()
+    ax1.set_xlim(data["sf"].min(),data["sf"].max())
+    ax1.set_xticks(data["sf"])
     numTransc = int(data[data["sf"]==1.0]["NumTranscripts"]/120)
     caption = "Figure. Plot shows the change in Kendall's tau ranking correlation coefficient between the ranking of transcripts by expression levels(TPM) using all reads in the alignment and the ranking of same transcripts using a portion of aligned reads. Assembly and expression estimation was performed using stringtie. Original number of aligned reads is "+str(spotsOriginal)+". Number of transcripts is "+str(numTransc)+"."
-    plt.figtext(.05, .05, caption,wrap=True,fontsize=18)
+    # plt.figtext(.05, .05, caption,wrap=True,fontsize=18)
     plt.savefig(outDir+"/png/tauSF.png")
 
 def plotScattermatrixSF(data,outDir,res):
@@ -60,10 +80,12 @@ def plotScattermatrixSF(data,outDir,res):
     # The data was visually inspected to determine any signs of linearity
     ax=sbn.pairplot(data[['sf','falsePositives','falseNegatives','falseNegativesFull','std','tauFull','median']])
     caption = "Figure. Pairplot of ... The diagonal shows distibutions of ..."
-    plt.figtext(.05, .05, caption,wrap=True,fontsize=18)
+    # plt.figtext(.05, .05, caption,wrap=True,fontsize=18)
     plt.savefig(outDir+"/png/scatterMatrixSF.png")
 
 def plotPCA(data,outDir,res):
+    plt.close("all")
+    plt.clf()
     Y = data["sf"]
     X = data[["falsePositives","falseNegatives","falseNegativesFull","median","weightedNormalizedNumExtremes","std","tauFull"]]
     X_std = StandardScaler().fit_transform(X)
@@ -71,27 +93,52 @@ def plotPCA(data,outDir,res):
     sklearn_pca = sklearnPCA(n_components=2)
     Y_sklearn = sklearn_pca.fit_transform(X_std)*-1
 
-    with plt.style.context('seaborn-whitegrid'):
-        plt.figure(figsize=(6, 4))
-        plt.scatter(Y.tolist(),Y_sklearn[:,0].tolist())
-        plt.xlabel('Sampling Factor')
-        plt.ylabel('Principal Component')
-        plt.legend(loc='lower center')
-        plt.tight_layout()
-
     xs=Y_sklearn[:,0]
     ys=Y_sklearn[:,1]*-1
-    n=sklearn_pca.components_.shape[1]
     labels = ["falsePositives","falseNegatives","falseNegativesFull","median","weightedNormalizedNumExtremes","std","tauFull"]
-    plt.figure(figsize=(30,30))
-    plt.scatter(xs,ys)
+    fig = plt.figure(figsize=(int(res[0]),int(res[1])))
+    ax1 = fig.add_axes((0.1,0.1,0.8,0.85))
+    ax1.set_title("PCA1 versus PCA2")
+    ax1.set_ylabel("PCA2")
+    ax1.set_xlabel("PCA1")
+    ax1.scatter(xs,ys)
+
+    n=sklearn_pca.components_.shape[1]
     for i in range(n):
-        plt.arrow(0, 0, sklearn_pca.components_[0,i]*-1, sklearn_pca.components_[1,i],color='r',alpha=0.5)
-        plt.text(sklearn_pca.components_[0,i]*-1.15, sklearn_pca.components_[1,i] * 1.15, labels[i], color='g', ha='center', va='center',size=26)
+        ax1.arrow(0, 0, sklearn_pca.components_[0,i]*-1, sklearn_pca.components_[1,i],color='r',alpha=1.0)
+        ax1.annotate(labels[i],(sklearn_pca.components_[0,i]*-1.15, sklearn_pca.components_[1,i] * 1.15))
+    
+    ticks2F = [str('%.2f' %((elem*spotsOriginal)/1000000))+"M" for elem in data["sf"].tolist()]
+    for i, txt in enumerate(data["sf"].tolist()):
+        ax1.annotate(str(txt)+" ("+ticks2F[i]+")", (xs[i],ys[i]))
+
+
     plt.savefig(outDir+"/png/pcaSF.png")
 
+def plotPrecision_VS_Recall(data,outDir,res):
+    plt.close("all")
+    plt.clf()
+    plt.figure(figsize=(int(res[0]),int(res[1])))
+    plt.title("Recall vs Precision")
+    fig, ax = plt.subplots()
+    ax.scatter(data["recall"], data["precision"])
+    ax.set_title("Recall versus Precision")
+    ax.set_xlabel("Recall")
+    ax.set_ylabel('Precision')
+    minXTick = data["recall"].min()-0.0001*(data["recall"].min())
+    maxXTick = data["recall"].max()+0.0001*(data["recall"].min())
+    ax.set_xlim(minXTick,maxXTick)
+    minYTick = data["precision"].min()-0.01*(data["precision"].min())
+    maxYTick = data["precision"].max()+0.01*(data["precision"].min())
+    ax.set_ylim(minYTick,maxYTick)
+
+    ticks2F = [str('%.2f' %((elem*spotsOriginal)/1000000))+"M" for elem in data["sf"].tolist()]
+    for i, txt in enumerate(data["sf"].tolist()):
+        ax.annotate(str(txt)+" ("+ticks2F[i]+")", (data["recall"].tolist()[i],data["precision"].tolist()[i]))
+
+    plt.savefig(outDir+"/png/recallPrecision.png")
+
 def plotAll(data,outDir,res,iqrCoefficient,gif):
-    print(len(data))
     try:
         iqrC = float(iqrCoefficient)
         # lets try identifying upper outliers in covBase
@@ -116,14 +163,21 @@ def plotAll(data,outDir,res,iqrCoefficient,gif):
             except:
                 print("Seems the coverage parameter is specified incorectly: ", sys.exc_info())
 
-    print(len(data))
     eDF = pd.DataFrame(data["ID"])
     for cl in list(data)[1:-1]:
-        eDF[str(cl)] = pd.DataFrame(preprocessing.scale(boxcox(data[cl]+1)[0]))
+        if cl == "covBase":
+            eDF[str(cl)] = pd.DataFrame(preprocessing.scale(boxcox(data[cl]+1)[0]))
+        else:
+            eDF[str(cl)] = data[str(cl)]
     eDF["sf"]=data['sf']
     eDF.to_csv(outDir+"/csv/groupedByIDTransformed.csv")
 
-    fig = plt.figure()
+    tickXCovBase = np.arange(min(data["covBase"]), max(data["covBase"])+1, (max(data["covBase"])-min(data["covBase"]))/20).tolist()
+    tickXCovBaseT = np.arange(min(eDF["covBase"]), max(eDF["covBase"])+1, (max(eDF["covBase"])-min(eDF["covBase"]))/20).tolist()
+    tickYExp = np.arange(min(data["tpmQ25"]), max(data["tpmQ75"])+1, (max(data["tpmQ75"])-min(data["tpmQ25"]))/20).tolist()
+    tickYExpT = np.arange(min(data["tpmQ50"]), max(data["tpmQ50"])+1, (max(data["tpmQ50"])-min(data["tpmQ50"]))/20).tolist()
+
+    fig = plt.figure(figsize=(int(res[0]),int(res[1])))
     ims = []
     line, = plt.plot([], [], lw=0.25)
     plt.xlim(eDF["covBase"].min(), eDF["covBase"].max())
@@ -135,9 +189,11 @@ def plotAll(data,outDir,res,iqrCoefficient,gif):
         spotsRetained = spotsOriginal*sfs[i]
         ax.set_xlabel("Change in median of transcript expression levels(TPM) at "+str(sfs[i]*100)+"%% of original numner of spots or mean of "+str(int(spotsRetained))+" spots across 12 alignments")
         ax.set_ylabel('Deviation of expression estimate from control (%TPM)')
+        ax.set_xticks(tickXCovBaseT)
+        ax.set_yticks(tickYExpT)
         return line,
 
-    fig2 = plt.figure()
+    fig2 = plt.figure(figsize=(int(res[0]),int(res[1])))
     ims2 = []
     line1, = plt.plot([], [], lw=0.25)
     plt.xlim(data["covBase"].min(), data["covBase"].max())
@@ -149,9 +205,11 @@ def plotAll(data,outDir,res,iqrCoefficient,gif):
         spotsRetained = spotsOriginal*sfs[i]
         ax.set_xlabel("Change in median of transcript expression levels(TPM) at "+str(sfs[i]*100)+"%% of original numner of spots or mean of "+str(int(spotsRetained))+" spots across 12 alignments")
         ax.set_ylabel('Deviation of expression estimate from control (%TPM)')
+        ax.set_xticks(tickXCovBase)
+        ax.set_yticks(tickYExp)
         return line1,
 
-    fig3 = plt.figure()
+    fig3 = plt.figure(figsize=(int(res[0]),int(res[1])))
     ims3 = []
     line2, = plt.plot([], [], lw=0.25)
     plt.xlim(data["covBase"].min(), data["covBase"].max())
@@ -161,16 +219,16 @@ def plotAll(data,outDir,res,iqrCoefficient,gif):
         line2.set_ydata(ims3[i][1])
         ax = line2.get_axes()
         spotsRetained = spotsOriginal*sfs[i]
-        caption = "Figure. Number of false negatives reported across all random downsamplings to "+str(sf*100)+"%% spots retained for each transcript."
+        caption = "Figure. Number of false negatives reported across all random downsamplings to "+str(sfs[i]*100)+"%% spots retained for each transcript."
         ax.set_xlabel(caption)
         ax.set_ylabel('Number of false negatives')
+        ax.set_xticks(tickXCovBase)
         return line2,
 
     unique=data["sf"].unique().tolist()
     for sf in unique:
-        print(sf)
+        print("Plotting: ",sf)
         dataTMP = data[data["sf"]==sf]
-        print(len(dataTMP))
 
         plt.close("all")
         ax=sbn.pairplot(dataTMP[['covBase','falseNegative','tpmMEAN','tpmQ50','tpmIQR','tpmSTD']])
@@ -184,6 +242,10 @@ def plotAll(data,outDir,res,iqrCoefficient,gif):
         plt.clf()
         plt.figure(figsize=(int(res[0]), int(res[1])))
         plt.title('Normalized TPM Deviation')
+        plt.xticks(np.arange(min(dataTMP["covBase"]), max(dataTMP["covBase"])+1, (max(dataTMP["covBase"])-min(dataTMP["covBase"]))/20).tolist())
+        plt.yticks(np.arange(min(dataTMP["tpmQ25"]), max(dataTMP["tpmQ75"])+1, (max(dataTMP["tpmQ75"])-min(dataTMP["tpmQ25"]))/20).tolist())
+        plt.ylabel('Deviation of expression estimate from control (% TPM)')
+        plt.xlabel("Transcript Coverage")
         plt.plot(dataTMP["covBase"], dataTMP["tpmQ50"],'k',color='#CC4F1B',lw=0.25)
         plt.fill_between(dataTMP["covBase"], dataTMP["tpmQ25"], dataTMP["tpmQ75"],
             alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
@@ -198,11 +260,13 @@ def plotAll(data,outDir,res,iqrCoefficient,gif):
         plt.clf()
         plt.figure(figsize=(int(res[0]), int(res[1])))
         plt.title('Change in median, 2nd and 3rd quartiles of transcript expression levels(TPM)')
+        plt.xticks(np.arange(min(eDF["covBase"]), max(eDF["covBase"])+1, (max(eDF["covBase"])-min(eDF["covBase"]))/20).tolist())
+        plt.yticks(np.arange(min(eDF["tpmQ50"]), max(eDF["tpmQ50"])+1, (max(eDF["tpmQ50"])-min(eDF["tpmQ50"]))/20).tolist())
         plt.ylabel('Deviation of expression estimate from control (% TPM)')
-        plt.xlabel("Portion of aligned spots")
+        plt.xlabel("Transcript Coverage")
         plt.plot(eDF[eDF["sf"]==sf]["covBase"], eDF[eDF["sf"]==sf]["tpmQ50"],'k',color='#CC4F1B',lw=0.25)
         spotsRetained = spotsOriginal*sf
-        caption = "Figure. Change in median of transcript expression levels(TPM) at "+str(sf*100)+"%% of original numner of spots or mean of "+str(int(spotsRetained))+" spots across 12 alignments. A BoxCoxTransformation was applied to coverage and tpm values for the purpose of normalization"
+        caption = "Figure. Change in median of transcript expression levels(TPM) at "+str(sf*100)+"%% of original numner of spots or mean of "+str(int(spotsRetained))+" spots across 12 alignments. A BoxCoxTransformation was applied to coverage values to normalize the distribution."
         plt.figtext(.02, .02, caption,wrap=True)
         plt.savefig(outDir+"/png/"+str(sf)+"boxIDTransformed.png")
         if gif == True:
@@ -214,6 +278,7 @@ def plotAll(data,outDir,res,iqrCoefficient,gif):
         plt.title("Number of false negatives versus coverage")
         plt.ylabel('Number of false negatives')
         plt.xlabel("Coverage")
+        plt.xticks(np.arange(min(dataTMP["covBase"]), max(dataTMP["covBase"])+1, (max(dataTMP["covBase"])-min(dataTMP["covBase"]))/20).tolist())
         plt.plot(dataTMP["covBase"], dataTMP["falseNegative"],'k',color='#CC4F1B',lw=0.25)
         spotsRetained = spotsOriginal*sf
         caption = "Figure. Number of false negatives reported across all random downsamplings to "+str(sf*100)+"%% spots retained for each transcript."
@@ -277,7 +342,9 @@ def main(args):
                     "tauTop50",
                     "tauBottom10",
                     "tauBottom20",
-                    "tauBottom50"]
+                    "tauBottom50",
+                    "recall",
+                    "precision"]
 
         dataSF = pd.read_csv(os.path.abspath(args.sf)).drop("Unnamed: 0",axis=1)
         dataSF.columns = headersSF
@@ -285,6 +352,7 @@ def main(args):
         plotTauSF(dataSF,os.path.abspath(args.out),args.resolution.split(":"))
         plotScattermatrixSF(dataSF,os.path.abspath(args.out),args.resolution.split(":"))
         plotPCA(dataSF,os.path.abspath(args.out),args.resolution.split(":"))
+        plotPrecision_VS_Recall(dataSF,os.path.abspath(args.out),args.resolution.split(":"))
 
     if not args.id == None:
         headersID = ['ID',
